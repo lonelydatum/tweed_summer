b793ed7fd121b627bd2f19e0d5199f1982d39077
Gar <gar@lonelydatum.com>
2022-05-27 16:16:05 -0400

    ON

--- dev/DSLimSplash_300x250/_js/main.js
+++ dev/DSLimSplash_300x250/_js/main.js
@@ -22,7 +22,7 @@ function start(){
 	tl.from(".hand", {duration:.3, x:"+=130", ease:Power3.easeOut})
 	tl.from(["._swirl", "._lime-bottom", "._lime-top"], {duration:.4, scale:0, ease:Back.easeOut})
 	tl.from(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], {duration:.4, scale:0, ease:Back.easeOut})
-	tl.from(["._lemon_main"], {duration:.4, scale:0, ease:Back.easeOut})
+	tl.from(["._lemon-main"], {duration:.4, scale:0, ease:Back.easeOut})
 
 	tl.from(".t1", {duration:.65, scale:1, opacity:0, ease:Back.easeIn})
 	tl.from(".cta", {duration:.3, opacity:0, ease:Power3.easeOut}, "+=.3")
