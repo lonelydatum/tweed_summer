a80987defd99a04b0c553580f990960ef163308a
Gar <gar@lonelydatum.com>
2022-05-30 10:39:34 -0400

    ON

--- dev/DSLimSplash_300x250/_bundled/main.js
+++ dev/DSLimSplash_300x250/_bundled/main.js
@@ -7,27 +7,28 @@ function start() {
 
 	var tl = new TimelineMax({ onComplete: function onComplete() {} });
 
+	var looper = { duration: .3, yoyo: true, repeat: 15, ease: Linear.easeNone, rotation: "+=16" };
+
 	tl.set(".frame1", { opacity: 1 });
 
 	tl.from(".t1", { duration: .65, scale: 1.5, opacity: 0, ease: Back.easeIn });
 
 	tl.from(".bg", { duration: .4, opacity: 0, ease: Power3.easeIn });
 	tl.from(".hand", { duration: .3, x: "+=130", ease: Power3.easeOut });
+	tl.from(".cta", { duration: .3, opacity: 0, ease: Power3.easeOut }, "-=.3");
+
 	tl.from(["._swirl", "._lime-bottom", "._lime-top"], { duration: .4, scale: 0, ease: "custom", onComplete: function onComplete() {
 			console.log('lkj');
-			TweenLite.to(["._swirl", "._lime-bottom", "._lime-top"], { duration: .3, yoyo: true, repeat: 8, ease: Linear.easeNone, rotation: "+=11" });
+			TweenLite.to(["._swirl", "._lime-bottom", "._lime-top"], looper);
 		} });
 	tl.from(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], { duration: .4, scale: 0, ease: "custom", onComplete: function onComplete() {
-			TweenLite.to(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], { duration: .3, yoyo: true, repeat: 8, ease: Linear.easeNone, rotation: "+=11" });
+			TweenLite.to(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], looper);
 		} });
+
 	tl.from(["._lemon-main"], { duration: .4, scale: 0, ease: "custom" });
 
 	tl.to(".t1", { duration: .2, opacity: 0, ease: Power3.easeOut }, 4.3);
 	tl.from(".t2", { duration: .65, scale: 1.5, opacity: 0, ease: "custom" });
-
-	tl.from(".cta", { duration: .3, opacity: 0, ease: Power3.easeOut }, "+=.3");
-
-	tl.from([".logo", ".legal"], { duration: .3, opacity: 0, ease: Power3.easeOut }, "+=.5");
 }
 
 start();
--- dev/DSLimSplash_300x250/_js/main.js
+++ dev/DSLimSplash_300x250/_js/main.js
@@ -16,7 +16,7 @@ function start(){
 		
 	}})
 
-	
+	const looper = {duration:.3, yoyo:true, repeat:15, ease:Linear.easeNone, rotation:"+=16"}
 
 	tl.set(".frame1", {opacity:1})
 
@@ -26,27 +26,24 @@ function start(){
 
 	tl.from(".bg", {duration:.4, opacity:0, ease:Power3.easeIn})
 	tl.from(".hand", {duration:.3, x:"+=130", ease:Power3.easeOut})
+	tl.from(".cta", {duration:.3, opacity:0, ease:Power3.easeOut}, "-=.3")
+
 	tl.from(["._swirl", "._lime-bottom", "._lime-top"], {duration:.4, scale:0, ease:"custom", onComplete:()=>{
 		console.log('lkj');
-		TweenLite.to(["._swirl", "._lime-bottom", "._lime-top"], {duration:.3, yoyo:true, repeat:8, ease:Linear.easeNone, rotation:"+=11"})
+		TweenLite.to(["._swirl", "._lime-bottom", "._lime-top"], looper)
 
 	}})
 	tl.from(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], {duration:.4, scale:0, ease:"custom", onComplete:()=>{
-		TweenLite.to(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], {duration:.3, yoyo:true, repeat:8, ease:Linear.easeNone, rotation:"+=11"})
+		TweenLite.to(["._lemon-top", "._lemon-ring", "._lemon-right", "._lemon-left"], looper)
 
 	}})
+
 	tl.from(["._lemon-main"], {duration:.4, scale:0, ease:"custom"})
-
-
-
 	
 	tl.to(".t1", {duration:.2,  opacity:0, ease:Power3.easeOut}, 4.3)
 	tl.from(".t2", {duration:.65, scale:1.5, opacity:0, ease:"custom"})
 	
-	tl.from(".cta", {duration:.3, opacity:0, ease:Power3.easeOut}, "+=.3")
-
-
-	tl.from([".logo", ".legal"], {duration:.3, opacity:0, ease:Power3.easeOut}, "+=.5")
+	
 
 
 
